9e029a486b672440e514c206a83df838614697bf
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2023-11-19 21:10:46 +0900

    init

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,5 @@
+<footer class="bg-blue-400 p-12 bottom-0 w-full">
+  <div class="container text-center mx-auto">
+    <p class="text-sm">build by GatoLife</p>
+  </div>
+</footer>
---
+++ src/lib/Portfolio.svelte
@@ -0,0 +1,35 @@
+<script>
+  let projects = [
+    {
+      name: "AVAX Messenger",
+      url: "https://avax-messenger-b2jpmijxu-gatolife-creator.vercel.app/",
+    },
+    {
+      name: "ETH NFT Collection",
+      url: "https://eth-nft-collection-starter-project-i7m3h4j9l-gatolife-creator.vercel.app/",
+    },
+    {
+      name: "ETH dApp",
+      url: "https://eth-nft-collection-starter-project-i7m3h4j9l-gatolife-creator.vercel.app/",
+    },
+  ];
+</script>
+
+<section id="portfolio">
+  <div class="container mx-auto">
+    <div class="py-32">
+      <h1 class="text-center mb-10 font-medium text-3xl">PORTFOLIO</h1>
+      <div class="flex flex-wrap lg:w-4/5 sm:mx-auto">
+        {#each projects as project}
+          <div class="p-2 md:w-1/3 w-full">
+            <div
+              class="bg-gray-400 text-white hover:bg-gray-800 cursor-pointer rounded p-4 text-center"
+            >
+              <a href={project.url}>{project.name}</a>
+            </div>
+          </div>
+        {/each}
+      </div>
+    </div>
+  </div>
+</section>
